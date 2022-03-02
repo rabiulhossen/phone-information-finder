e5dffd9b0a6544f2ea59f0328ab24f5c009935b2
rabiul-hossen <rhsimon69@gmail.com>
2022-03-02 16:17:51 +0600

    details

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const showMobile = (phones) => {
         const parent = document.getElementById("list-container");
         const div = document.createElement('div');
         div.innerHTML = ` <div class="container">
-    <div class="row">
+    <div>
         <div class="card border rounded col-md-4 m-3  pt-3 ps-5 shadow" style="width: 18rem;">
             <img src="${phone.image}" class="card-img-top w-75" alt="...">
             <div class="card-body">
@@ -24,33 +24,33 @@ const showMobile = (phones) => {
                 <a href="#" class="btn btn-success fw-bold"onclick="details()">DETAILS</a>
             </div>
         </div>
-        <div class="card border rounded col-md-4 m-3 pt-3 ps-5 shadow" style="width: 18rem;">
-            <img src="${phone.image}" class="card-img-top w-75" alt="...">
-            <div class="card-body">
-                <h5 class="card-title fw-bold">Model:${phone.phone_name}</h5>
-                <p class="card-text fw-bold">BRAND:${phone.brand}</p>
-                <a href="#" class="btn btn-success fw-bold"onclick="details()">DETAILS</a>
-            </div>
-        </div>
-        <div class="card border rounded col-md-4 m-3  pt-3 ps-5 shadow" style="width: 18rem;">
-            <img  src="${phone.image}" class="card-img-top w-75" alt="...">
-            <div class="card-body">
-                <h5 class="card-title fw-bold">Model:${phone.phone_name}</h5>
-                <p class="card-text fw-bold">BRAND:${phone.brand}</p>
-                <a href="#" class="btn btn-success fw-bold" onclick="details('${phone.slug}')">DETAILS</a>
-            </div>
-        </div>
+
     </div> 
     </div>`;
         parent.appendChild(div);
 
     }
-
 }
-
 const details = (info) => {
     const detailsLink = ` https://openapi.programming-hero.com/api/phone/${info}`;
     fetch(detailsLink)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => showDetails(data.data.mainFeatures));
+
 }
+const showDetails = (info) => {
+    const container = document.getElementById("details-container").innerHTML = `<div class="card" style="width: 18rem;">
+    
+    <div class="card-body">
+        <h5 class="card-title fw-bold">Model:${info.mainFeatures.storage}</h5>
+        <p class="card-text fw-bold">BRAND:${info.mainFeatures.displaySize}</p>
+       
+        <p class="card-text fw-bold">BRAND:${info.mainFeatures.chipSet}</p>
+       
+        <p class="card-text fw-bold">BRAND:${info.mainFeatures.memory}</p>
+       
+      <a href="#" class="btn btn-info">Go somewhere</a>
+    </div>
+  </div>`;
+
+};
