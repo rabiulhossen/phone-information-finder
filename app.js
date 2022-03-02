42e300e6c4a48e939ed0b3a2a410116a4b3904b6
rabiul-hossen <rhsimon69@gmail.com>
2022-03-02 12:26:49 +0600

    card-responsive

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
 const allMobile = () => {
     const searchValue = document.getElementById("search-box").value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=$"{phone_name}`;
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+
 
     fetch(url)
-        .then((response) => response.json())
-        .then((data) => showMobile(data.brand));
+        .then(response => response.json())
+        .then(data => showMobile(data.data));
 };
 
-const showMobile = (players) => {
+const showMobile = (phones) => {
 
-    for (const player of players) {
+    for (const phone of phones) {
         const parent = document.getElementById("list-container");
         const div = document.createElement('div');
         div.innerHTML = ` <div class="container">
     <div class="row">
-        <div class="card border" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+        <div class="card border rounded col-md-4 m-3  pt-3 ps-5 shadow" style="width: 18rem;">
+            <img src="${phone.image}" class="card-img-top w-75" alt="...">
             <div class="card-body">
-                <h5 class="card-title"> title</h5>
-                <p class="card-text">Some text here</p>
-                <a href="#" class="btn btn-primary">button</a>
+                <h5 class="card-title fw-bold">Model:${phone.phone_name}</h5>
+                <p class="card-text fw-bold">BRAND:${phone.brand}</p>
+                <a href="#" class="btn btn-success fw-bold">DETAILS</a>
             </div>
         </div>
-    </div>
-</div>
-`;
+        <div class="card border rounded col-md-4 m-3 pt-3 ps-5 shadow" style="width: 18rem;">
+            <img src="${phone.image}" class="card-img-top w-75" alt="...">
+            <div class="card-body">
+                <h5 class="card-title fw-bold">Model:${phone.phone_name}</h5>
+                <p class="card-text fw-bold">BRAND:${phone.brand}</p>
+                <a href="#" class="btn btn-success fw-bold">DETAILS</a>
+            </div>
+        </div>
+        <div class="card border rounded col-md-4 m-3  pt-3 ps-5 shadow" style="width: 18rem;">
+            <img  src="${phone.image}" class="card-img-top w-75" alt="...">
+            <div class="card-body">
+                <h5 class="card-title fw-bold">Model:${phone.phone_name}</h5>
+                <p class="card-text fw-bold">BRAND:${phone.brand}</p>
+                <a href="#" class="btn btn-success fw-bold">DETAILS</a>
+            </div>
+        </div>
+    </div> 
+    </div>`;
         parent.appendChild(div);
-        console.log(player);
+
     }
 
 }
